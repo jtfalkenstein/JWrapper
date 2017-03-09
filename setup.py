2f1964cbe5fbef7c2f791948ed71fe47bbfd53b2
Jon Falkenstein <jtfalkenstein@gmail.com>
2017-03-09 10:31:59 -0600

    updating property checking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='jwrapper',
-    version='1.1.13',
+    version='1.1.14',
     description='A small Python tool to make console inspection SO MUCH EASIER.',
     author='Jon Falkenstein',
     author_email='jtfalkenstein@gmail.com',
